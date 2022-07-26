535ee687a06e383e0019ddcd1f536c7e315f43d1
Ngyyyennnn <54529371+tqnguyen19it3@users.noreply.github.com>
2022-07-26 07:53:47 +0700

    bs4

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 1234;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// template engine
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
